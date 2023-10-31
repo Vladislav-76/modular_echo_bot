87b8e32a53a168272a82733177f4d3e4415541e7
Vladislav-76 <sillmaril2@gmail.com>
2023-10-31 17:08:35 +0300

    Routers added

--- bot.py
+++ bot.py
@@ -2,12 +2,15 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from config_data import config
+from handlers import other_handlers, user_handlers
 
 
 async def main() -> None:
     bot = Bot(token=config.token)
     admin_ids = config.admin_ids
     dp = Dispatcher()
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
